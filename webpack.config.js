0047e763ec2a0a320f12cd6e010617eca062ed1a
yinzihang <201250173@smail.nju.edu.cn>
2022-07-18 11:34:15 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     mode: 'production',
-    entry: path.resolve(__dirname, './vm/index.js'),
+    entry: path.resolve(__dirname, './vm/VM.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'vm.js'
